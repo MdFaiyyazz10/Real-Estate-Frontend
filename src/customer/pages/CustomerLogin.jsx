cb6905687281c35aa6b707a18095129569d6e008
Faiyyazz <mdfaiyyazz397@gmail.com>
2024-10-09 11:35:38 +0530

    login update

--- src/customer/pages/CustomerLogin.jsx
+++ src/customer/pages/CustomerLogin.jsx
@@ -7,7 +7,7 @@ import toast from 'react-hot-toast';
 import axios from 'axios';
 import { backend } from '../../App';
 import { signInFailed, signInStart, signInSuccess } from '../../redux/user/userSlicer.js';
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 function CustomerLogin() {
     const [activeTab, setActiveTab] = useState('register'); // Track the active tab
@@ -17,11 +17,11 @@ function CustomerLogin() {
         phone: '',
         password: ''
     });
-    const [loginFormData , setLoginFormData] = useState({
+    const [loginFormData, setLoginFormData] = useState({
         email: '',
         password: ''
     });
-   
+
     const dispatch = useDispatch()
 
     const registerChangeHandler = (e) => {
@@ -115,42 +115,47 @@ function CustomerLogin() {
                         <div className='customer-rejister_login_forgotpass_btn'>
                             <h5>Create your account</h5>
                             <form onSubmit={registerSubmitHandler}>
-                                {/* <label htmlFor="">Sponsor ID :</label>
-                                <input type="text" required />
-                                <br /> */}
-                                <label htmlFor="">Full Name :</label>
-                                <input type="text" name='name' required onChange={registerChangeHandler} />
+                                <div>
+                                    <label htmlFor="">Sponsor ID :</label>
+                                    <input placeholder='Enter Sponsor ID:' type="text" required />
+
+                                    <label htmlFor="">Full Name :</label>
+                                    <input placeholder='Enter Full Name:' type="text" name='name' required onChange={registerChangeHandler} />
+                                </div>
+
                                 <div>
                                     <div>
                                         <label htmlFor="">Email Address :</label>
-                                        <input type="email" name='email' required onChange={registerChangeHandler} />
+                                        <input placeholder='Enter Email Address:' type="email" name='email' required onChange={registerChangeHandler} />
                                     </div>
                                     <div>
                                         <label htmlFor="">Phone Number :</label>
-                                        <input type="number" name='phone' required onChange={registerChangeHandler} />
+                                        <input placeholder='Enter Phone No:' type="number" name='phone' required onChange={registerChangeHandler} />
                                     </div>
                                 </div>
+
                                 <div>
                                     <div>
                                         <label htmlFor="">Set Password :</label>
-                                        <input type="password" name='password' required onChange={registerChangeHandler} />
+                                        <input placeholder='Enter Password:' type="password" name='password' required onChange={registerChangeHandler} />
                                     </div>
-                                    {/* <div>
+                                    <div>
                                         <label htmlFor="">Role Type :</label>
-                                        <input type="number" required />
-                                    </div> */}
+                                        <input placeholder='Enter Role Type::' type="number" required />
+                                    </div>
                                 </div>
+
                                 <div>
-                                    {/* <div>
+                                    <div>
                                         <label htmlFor="">Adhar Number :</label>
-                                        <input type="email" required />
-                                    </div> */}
-                                    {/* <div>
+                                        <input placeholder='Enter Adhar Number:' type="email" required />
+                                    </div>
+                                    <div>
                                         <label htmlFor="">Pan Number :</label>
-                                        <input type="number" required />
-                                    </div> */}
+                                        <input placeholder='Enter Pan Number:' type="number" required />
+                                    </div>
                                 </div>
-                                <br />
+
                                 <button className="home-hero1_btn"> Register  </button>
                             </form>
                         </div>
@@ -160,12 +165,14 @@ function CustomerLogin() {
                         <div className='customer-rejister_login_forgotpass_btn'>
                             <h5>Sign into your account</h5>
                             <form onSubmit={loginSubmitHandler} >
-                                <label >E-mail :</label>
-                                <input type="email" required placeholder='Enter your E-mail' name='email' onChange={loginChangeHandler} />
-                                <br />
-                                <label >Password :</label>
-                                <input type="password" required placeholder='Enter Your Password' name='password' onChange={loginChangeHandler} />
-                                <br />
+                                <div>
+                                    <label >E-mail :</label>
+                                    <input type="email" required placeholder='Enter your E-mail' name='email' onChange={loginChangeHandler} />
+
+                                    <label >Password :</label>
+                                    <input type="password" required placeholder='Enter Your Password' name='password' onChange={loginChangeHandler} />
+                                </div>
+
                                 <button className="home-hero1_btn"> Login  </button>
                             </form>
                         </div>
@@ -175,9 +182,12 @@ function CustomerLogin() {
                         <div className='customer-rejister_login_forgotpass_btn'>
                             <h5>Forgot your password</h5>
                             <form>
-                                <label htmlFor="">Registered E-Mail Id :</label>
-                                <input type="password" required />
-                            <button className="home-hero1_btn"> Send OTP  </button>
+                                <div>
+                                    <label htmlFor="">Registered E-Mail Id :</label>
+                                    <input placeholder='Enter Your Password:' type="password" required />
+                                </div>
+
+                                <button className="home-hero1_btn"> Send OTP  </button>
                             </form>
                         </div>
                     }
